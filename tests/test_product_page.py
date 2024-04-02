28fec8d1442ab7c3ff8f039afdb63c5249279e73
NORBIT\Yana.Shatunova <Yana.Shatunova@norbit.ru>
2024-04-02 18:34:23 +0300

    Задание: добавление в корзину со страницы товара

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,14 +1,14 @@
 import time
 from pages.main_page import MainPage
-from pages.login_page import LoginPage
+from pages.product_page import ProductPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    url = "http://selenium1py.pythonanywhere.com/"
+    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     main_page = MainPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    login_page = LoginPage(browser, url)
+    product_page = ProductPage(browser, url)
     main_page.open()                      # открываем страницу
-    main_page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    main_page.should_be_login_link()
     time.sleep(2)
-    login_page.should_be_login_page()
+    product_page.add_product()
+    time.sleep(1)
+    main_page.solve_quiz_and_get_code()
