4d79d25e20ec64ca27244ac7a16b32a517003e6a
NORBIT\Yana.Shatunova <Yana.Shatunova@norbit.ru>
2024-04-02 18:44:24 +0300

    Задание: независимость от данных

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -4,11 +4,14 @@ from pages.product_page import ProductPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
     main_page = MainPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     product_page = ProductPage(browser, url)
     main_page.open()                      # открываем страницу
     time.sleep(2)
     product_page.add_product()
-    time.sleep(1)
+    time.sleep(2)
     main_page.solve_quiz_and_get_code()
+    time.sleep(2)
+    product_page.check_add_notify("The shellcoder's handbook has been added to your basket.")
+    product_page.check_basket_total("Your basket total is now £9.99")
